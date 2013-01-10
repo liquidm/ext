01b4ce25e5d8de1520fe53c6f06f344d63819560
Benedikt Böhm <benedikt.boehm@madvertise.com>
2013-01-10 14:58:07 +0100

    do not start reactor over and over again

--- lib/servolux/eventmachine.rb
+++ lib/servolux/eventmachine.rb
@@ -15,23 +15,24 @@ module Servolux
       end
     end
 
+    def before_starting
+      @reactor = Thread.new do
+        begin
+          EventMachine.run { boot if respond_to?(:boot) }
+        rescue => e
+          $log.exception(e)
+        end
+      end
+    end
+
     def after_stopping
+      $log.debug("eventmachine stop")
       EventMachine.stop_event_loop if EventMachine.reactor_running?
       @reactor.kill
       @reactor.join if @reactor.is_a?(Thread)
     end
 
     def run
-      unless EventMachine.reactor_running?
-        @reactor.join if @reactor.is_a?(Thread)
-        @reactor = Thread.new do
-          begin
-            EventMachine.run { boot }
-          rescue => e
-            $log.exception(e)
-          end
-        end
-      end
     end
 
   end
