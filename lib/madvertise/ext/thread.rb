0d5de11e6cd9f85523c302d5be6a78b8311bb721
Benedikt Böhm <bb@xnull.de>
2013-11-16 23:34:57 +0100

    abort on thread exceptions by default

--- lib/madvertise/ext/thread.rb
+++ lib/madvertise/ext/thread.rb
@@ -1,8 +1,17 @@
+# make default what should be default
+Thread.abort_on_exception = true
+
 class Thread
-  def name=(value)
-    self[:name] = value
+
+  def self.name
+    Thread.current[:name] || Java::JavaLang::Thread.currentThread.getName
+  end
+
+  def self.name=(value)
+    Thread.current[:name] = value
     if RUBY_PLATFORM == "java"
-      java.lang.Thread.currentThread.setName(value)
+      Java::JavaLang::Thread.currentThread.setName(value)
     end
   end
+
 end
