f4ef8a492b9a3626ced095630512cacee010c54d
Benedikt Böhm <bb@xnull.de>
2014-02-22 23:15:31 +0100

    make kafka tracker work

--- lib/liquid/tracker/kafka_tracker.rb
+++ lib/liquid/tracker/kafka_tracker.rb
@@ -4,6 +4,7 @@ require_relative '../scala-library-2.10.3.jar'
 require_relative '../metrics-core-2.2.0.jar'
 require_relative '../metrics-annotation-2.2.0.jar'
 require_relative '../kafka_2.10-0.8.0.jar'
+require_relative '../snappy-java-1.1.1-M1.jar'
 
 module Tracker
   class KafkaTracker < Base
@@ -32,6 +33,8 @@ module Tracker
 
     def shutdown
       @producer.close if @producer
+    rescue Java::KafkaProducer::ProducerClosedException
+      # pass
     end
 
   end
