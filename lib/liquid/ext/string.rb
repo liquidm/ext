81ae5659829d9b122d03dd173c2485779521e02f
Adam Dratwinski <adam.dratwinski@gmail.com>
2014-03-11 13:56:15 +0100

    string: add camel_case and snake_case methods

--- lib/liquid/ext/string.rb
+++ lib/liquid/ext/string.rb
@@ -8,4 +8,17 @@ class String
       self
     end
   end
+
+  # from: http://rubydoc.info/gems/extlib/0.9.15/String#camel_case-instance_method
+  def camel_case
+    return self if self !~ /_/ && self =~ /[A-Z]+.*/
+    split('_').map{|e| e.capitalize}.join
+  end
+
+  def snake_case
+    return downcase if match(/\A[A-Z]+\z/)
+    gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2').
+        gsub(/([a-z])([A-Z])/, '\1_\2').
+        downcase
+  end
 end
