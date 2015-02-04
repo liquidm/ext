175e586b895e45d34a952fe8487488c6d15288fa
Jakub Wroblewski <jakub.wroblewski@gmail.com>
2015-02-04 14:41:06 +0100

    tracker: Dispatch events to multiple topics

--- lib/liquid/tracker/base.rb
+++ lib/liquid/tracker/base.rb
@@ -1,4 +1,5 @@
 require 'liquid/tracker/json_serializer'
+java_import 'java.util.concurrent.ConcurrentHashMap'
 
 module Tracker
   class Base
@@ -6,10 +7,38 @@ module Tracker
 
     def initialize(dimensions = {})
       @dimensions = dimensions
+
+      @topics = ConcurrentHashMap.new
+      @topics_for_event = Hash.new{ |h, k| h[k] = [] }
+      @event_lookup = nil
     end
 
     def with_topic(topic, serializer = nil)
-      Topic.new(topic, self, serializer)
+      @topics["#{topic}#{serializer}"] ||= Topic.new(topic, self, serializer)
+    end
+
+    def dispatch(topic_events_map, serializer = nil, &event_lookup)
+      @topics_for_event.reject! { true }
+      @event_lookup = event_lookup
+
+      topic_events_map.each do |topic, events|
+        events.each do |event|
+          $log.info("tracker:dispatch", event: event, topic: topic)
+          @topics_for_event[event] << with_topic(topic.to_s, serializer)
+        end
+      end
+    end
+
+    def dispatch_event(event)
+      event_lookup = @event_lookup.call(event)
+
+      if (topics = @topics_for_event[event_lookup]).any?
+        topics.each do |topic|
+          topic.event(event)
+        end
+      else
+        $log.warn("tracker:unregistered_event", event: event_lookup)
+      end
     end
   end
 
